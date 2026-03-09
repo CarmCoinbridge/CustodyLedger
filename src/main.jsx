08723f6e593de2180c435142148872b10499a8a9
CarmCoinbridge <info@coinbridgefoundation.org>
2026-03-09 22:33:17 +1100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import MobileApp from './MobileApp.jsx'
+import DriverApp from './DriverApp.jsx'
+
+const path = window.location.pathname
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {path === '/mobile' ? <MobileApp /> : 
+     path === '/driver' ? <DriverApp /> : 
+     <App />}
   </React.StrictMode>
 )
